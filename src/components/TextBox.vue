10eb05a75030e89b73d01df05abc8085e171c6fa
Jason Poindexter <poindexter.json@gmail.com>
2019-03-13 15:43:12 -0700

    Resize text box. Add styling. Use static code snippet

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -1,22 +1,46 @@
 <template>
-  <textarea class='text-input'></textarea>
+  <textarea class='text-input' v-model="text"></textarea>
 </template>
 
 <script>
+// TODO: Put code files in static folder structure and import as string
+const snippet = 'const storage = require(\'../storage\');\n' +
+        '\n' +
+        'module.exports = async (req, res) => {\n' +
+        '    try {\n' +
+        '        const meta = await storage.metadata(req.params.id);\n' +
+        '        res.set(\'WWW-Authenticate\', `send-v1 ${meta.nonce}`);\n' +
+        '        res.send({\n' +
+        '            requiresPassword: meta.pwd\n' +
+        '        });\n' +
+        '    } catch (e) {\n' +
+        '        res.sendStatus(404);\n' +
+        '    }\n' +
+        '};\n'
+
 export default {
   name: 'TextBox',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      text: snippet
+
+    }
   }
 }
 </script>
 
 <style scoped>
-
   .text-input {
     height: 500px;
-    width: 600px;
+    width: 100%;
     background: grey;
+    color: white;
     border: none;
+    padding: 10px 10px 0;
+    font-family: droid,sans-serif;
+    font-size: 32px;
   }
 </style>
