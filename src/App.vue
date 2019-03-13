91858bf8ccc28b1da5d66f92e9f710b8907387a8
Jason Poindexter <poindexter.json@gmail.com>
2019-03-13 14:49:21 -0700

    Initial project creation / add text input area

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <TextBox/>
+  </div>
+</template>
+
+<script>
+import TextBox from './components/TextBox.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TextBox
+  }
+}
+</script>
+
+<style>
+#app {}
+</style>
---
+++ src/components/TextBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <textarea class='text-input'></textarea>
+</template>
+
+<script>
+export default {
+  name: 'TextBox',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+  .text-input {
+    height: 500px;
+    width: 600px;
+    background: grey;
+    border: none;
+  }
+</style>
